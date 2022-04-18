0e97ced43b6aefa8dda7ddb1380da15fd09fd57e
Ahmet <ahmetakkusx@gmail.com>
2022-04-18 11:12:51 +0200

    changes in awesome

---
+++ .config/awesome/themes/default/theme.lua
@@ -0,0 +1,90 @@
+---------------------------
+-- Default awesome theme --
+---------------------------
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+local theme = {}
+
+theme.font          = "Jetbrains Mono Extra Bold Nerd Font 9"
+
+theme.bg_normal     = "#000000"
+theme.bg_focus      = "#1f142b"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#000000"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#aaaaaa"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#444444"
+
+theme.useless_gap   = dpi(1)
+theme.border_width  = dpi(1)
+theme.border_normal = "#000000"
+theme.border_focus  = "#8f1fff"
+theme.border_marked = "#91231c"
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+-- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(5)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+theme.taglist_spacing = 5
+
+-- Variables set for theming notifications:
+-- notification_font
+-- notification_[bg|fg]
+-- notification_[width|height|margin]
+-- notification_[border_color|border_width|shape|opacity]
+theme.notification_width = 300
+theme.notification_height = 100
+theme.notification_font = "Jetbrains Mono Extra Bold Nerd Font 10"
+theme.notification_border_color = "#8f1fff"
+theme.notification_position = "bottom_right"
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(15)
+theme.menu_width  = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- You can use your own layout icons like this:
+theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+theme.layout_max = themes_path.."default/layouts/maxw.png"
+theme.layout_tile = themes_path.."default/layouts/tilew.png"
+
+theme.tasklist_disable_icon = true
+theme.gap_single_client = false
+theme.maximized_hide_border = true
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
